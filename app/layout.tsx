bf02d469e17a4b88a87a535dd7a7dcbf8c0fae4b
Mohamad Arleyxa Syarif <kiritonkichan@gmail.com>
2026-02-17 11:54:44 +0700

    update animasi pake framer motion

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ClientWrapper from "./components/ClientWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,7 +37,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
       >
         <Navbar />
-        {children}
+        <ClientWrapper>
+          {children}
+        </ClientWrapper>
         <Footer />
       </body>
     </html>
